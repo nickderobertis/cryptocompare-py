1ecc6c3d87e5f02d03f62f88988fde2a077b5c9c
Nick DeRobertis <whoopnip@gmail.com>
2019-10-14 15:56:17 -0400

    Restructure general parsing tools

--- cryptocompsdk/history/parse.py
+++ cryptocompsdk/history/parse.py
@@ -1,57 +1,9 @@
-from typing import TypeVar, Any, Callable, List, Type, cast, Optional
+from typing import Any, List, Optional
 
 import pandas as pd
 
-T = TypeVar("T")
-
-
-def from_int(x: Any) -> int:
-    assert isinstance(x, int) and not isinstance(x, bool)
-    return x
-
-
-def from_none(x: Any) -> Any:
-    assert x is None
-    return x
-
-
-def from_union(fs, x):
-    for f in fs:
-        try:
-            return f(x)
-        except:
-            pass
-    assert False
-
-
-def from_float(x: Any) -> float:
-    assert isinstance(x, (float, int)) and not isinstance(x, bool)
-    return float(x)
-
-
-def from_str(x: Any) -> str:
-    assert isinstance(x, str)
-    return x
-
-
-def to_float(x: Any) -> float:
-    assert isinstance(x, float)
-    return x
-
-
-def from_bool(x: Any) -> bool:
-    assert isinstance(x, bool)
-    return x
-
-
-def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
-    return [f(y) for y in x]
-
-
-def to_class(c: Type[T], x: Any) -> dict:
-    assert isinstance(x, c)
-    return cast(Any, x).to_dict()
+from cryptocompsdk.general.parse import from_int, from_none, from_union, from_float, from_str, to_float, from_bool, \
+    from_list, to_class
 
 
 class HistoryRecord:
