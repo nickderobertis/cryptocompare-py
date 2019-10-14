7c4f2e3aaa9dc8f5dde53032930c918457f499ec
Nick DeRobertis <whoopnip@gmail.com>
2019-10-14 17:00:45 -0400

    Update quicktype parse tools to stop hiding errors in parsing

--- cryptocompsdk/general/parse.py
+++ cryptocompsdk/general/parse.py
@@ -1,15 +1,45 @@
-from typing import Any, Callable, List, Type, cast, TypeVar
+from typing import Any, Callable, List, Type, cast, TypeVar, Dict
 
 T = TypeVar("T")
 
 
+class InvalidTypeException(Exception):
+    pass
+
+
+class NotNoneException(InvalidTypeException):
+    pass
+
+
+class NotIntException(InvalidTypeException):
+    pass
+
+
+class NotFloatException(InvalidTypeException):
+    pass
+
+
+class NotStrException(InvalidTypeException):
+    pass
+
+
+class NotBoolException(InvalidTypeException):
+    pass
+
+
+class NotListException(InvalidTypeException):
+    pass
+
+
 def from_int(x: Any) -> int:
-    assert isinstance(x, int) and not isinstance(x, bool)
-    return x
+    if isinstance(x, int) and not isinstance(x, bool):
+        return x
+    raise NotIntException(x)
 
 
 def from_none(x: Any) -> Any:
-    assert x is None
+    if x is not None:
+        raise NotNoneException(x)
     return x
 
 
@@ -17,36 +47,51 @@ def from_union(fs, x):
     for f in fs:
         try:
             return f(x)
-        except:
+        except InvalidTypeException:
             pass
     assert False
 
 
 def from_float(x: Any) -> float:
-    assert isinstance(x, (float, int)) and not isinstance(x, bool)
-    return float(x)
+    if isinstance(x, (float, int)) and not isinstance(x, bool):
+        return float(x)
+    raise NotFloatException(x)
 
 
 def from_str(x: Any) -> str:
-    assert isinstance(x, str)
-    return x
+    if isinstance(x, str):
+        return x
+    raise NotStrException(x)
 
 
 def to_float(x: Any) -> float:
-    assert isinstance(x, float)
-    return x
+    if isinstance(x, float):
+        return x
+    raise NotFloatException(x)
 
 
 def from_bool(x: Any) -> bool:
-    assert isinstance(x, bool)
-    return x
+    if isinstance(x, bool):
+        return x
+    raise NotBoolException(x)
 
 
 def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
-    return [f(y) for y in x]
+    if isinstance(x, list):
+        return [f(y) for y in x]
+    raise NotListException(x)
 
 
 def to_class(c: Type[T], x: Any) -> dict:
     assert isinstance(x, c)
     return cast(Any, x).to_dict()
+
+
+def is_type(t: Type[T], x: Any) -> T:
+    assert isinstance(x, t)
+    return x
+
+
+def from_dict(f: Callable[[Any], T], x: Any) -> Dict[str, T]:
+    assert isinstance(x, dict)
+    return { k: f(v) for (k, v) in x.items() }
